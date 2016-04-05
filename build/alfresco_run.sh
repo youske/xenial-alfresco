c320355ddf0246dffce8e6a7526a920f1c3596bf
youske miyakoshi <youske@gmail.com>
2016-04-05 09:28:40 +0900

    modify

--- build/alfresco_run.sh
+++ build/alfresco_run.sh
@@ -2,9 +2,9 @@
 
 ALFRESCO=/alfresco-community
 ${ALFRESCO}/alfresco.sh start
-
+cat >> /dev/null
 cat <<EOF >>~/.bashrc
 trap "${ALFRESCO}/lfresco.sh stop; exit 0" TERM
 EOF
 
-exec /bin/bash
+#exec /bin/bash
